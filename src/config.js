39f7bd4519664464170e3e2b70eb8ecfb16c50be
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-02-16 21:33:01 -0300

    config.js changes | to ||

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const env = {
-    emailUser: (process.env.EMAIL_USER | ""),
-    emailPassword: (process.env.EMAIL_PASS | ""),
-    emailHost: (process.env.EMAIL_HOST | "smtp.gmail.com"),
+    emailUser: (process.env.EMAIL_USER || ""),
+    emailPassword: (process.env.EMAIL_PASS || ""),
+    emailHost: (process.env.EMAIL_HOST || "smtp.gmail.com"),
     emailPort: (process.env.EMAIL_PORT = 465),
   };
